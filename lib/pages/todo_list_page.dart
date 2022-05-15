bc7c1c2039f43fdcf84fe4dcf5e980f0b5333b97
AdsonBruno <adsonbruno2@gmail.com>
2022-05-15 00:54:33 -0300

    finalizando botoes add e limpar tudo

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -11,21 +11,54 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              const Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Adicione uma tarefa',
-                    hintText: 'Ex.: Estudar Flutter',
+              Row(
+                children: [
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma tarefa',
+                        hintText: 'Ex.: Estudar Flutter',
+                      ),
+                    ),
                   ),
-                ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xFF00D7F3),
+                      padding: const EdgeInsets.all(14),
+                    ),
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(width: 8),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('+'),
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                children: [
+                  const Expanded(
+                    child: Text(
+                      'Você possui zero tarefas pendentes',
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xFF00D7F3),
+                      padding: const EdgeInsets.all(14),
+                    ),
+                    child: const Text('Limpar tudo'),
+                  ),
+                ],
               ),
             ],
           ),
